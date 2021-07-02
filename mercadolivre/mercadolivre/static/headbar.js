5a859434822cd1250e83fdb9351f6abb052de3e4
pietro.vanelli <pietroparaventivanelli@gmail.com>
2021-07-02 15:43:09 -0300

    correcao headbar

--- mercadolivre/mercadolivre/static/headbar.js
+++ mercadolivre/mercadolivre/static/headbar.js
@@ -7,11 +7,31 @@ document.getElementById('usuario').innerHTML = ""
 
 if (usuario == 'AnonymousUser'){
     document.getElementById('list').innerHTML = 
-        '<li><a href="/user/login/">Login</a></li>'
+        '<li id="index"><a href="/index/">Home</a></li>'+
+        '<li id="login"><a href="/user/login/">Login</a></li>'+
+        '<li id="signin"><a href="/user/signin/">Sign In</a></li>'
+    console.log(usuario)
+    pagina = document.getElementById('title').innerText
+    if (pagina === 'Home'){
+        document.getElementById('index').innerHTML = 
+        '<a href="/index/" id="index" class="active">Home</a>'
+    }
+    else if (pagina === 'Login'){
+        document.getElementById('login').innerHTML =
+        '<a href="/user/login/" class="active">Login</a>'
+    }
+    else if (pagina === 'Signin'){
+        document.getElementById('signin').innerHTML = 
+        '<a href="/user/signin/" class="active">Sign In</a>'
+    }
+    else{
+        console.log(pagina)
+    }
 }
 else{
     document.getElementById('list').innerHTML=
-        '<li><a href="/index/" id="UserLogado">Logado como: {{ usuario }}</a></li>' +
-        '<li><a href="/user/logoff">Logoff</a></li>'
+        '<li><a href="/index/" id="index">Home</a></li>'+
+        '<li><a href="/user/logoff">Logout</a></li>' +
+        '<li><a href="/index/" id="UserLogado">Logado como: '+ usuario +'</a></li>'
         console.log(page + " " + usuario)
 }
--- mercadolivre/user_app/views.py
+++ mercadolivre/user_app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import redirect, render
 from .forms import SignInForm, LoginForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, get_user, login, logout
 
 
 def login_view(request):
     form = LoginForm()
+    usuario = get_user(request)
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
@@ -15,9 +16,10 @@ def login_view(request):
                 return redirect('/')
             else:
                 print('login user dosent existis/invalid')
-    return render(request, 'login_page.html', {'form': form})
+    return render(request, 'login_page.html', {'form': form, 'usuario': usuario})
 
 def signin_view(request):
+    usuario = get_user(request)
     if request.method == 'POST':
         form = SignInForm(request.POST)
         if form.is_valid():
@@ -29,7 +31,9 @@ def signin_view(request):
             return redirect('/')
     else:
         form = SignInForm()
-    return render(request, 'signin_page.html', {'form': form})
+    return render(request, 'signin_page.html', {'form': form, 'usuario': usuario})
 
 def logoff_view(request):
-    return render(request, 'logoff_page.html')
+    usuario = get_user(request)
+    logout(request)
+    return render(request, 'logoff_page.html', {'usuario': usuario})
