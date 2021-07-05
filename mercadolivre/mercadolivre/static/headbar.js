644a4d261ee78b1ecf9d059fa3d1fc1600fe0dce
Pietro Paraventi Vanelli <pietroparaventivanelli@gmail.com>
2021-07-05 20:55:48 -0300

    update user info

--- mercadolivre/mercadolivre/static/headbar.js
+++ mercadolivre/mercadolivre/static/headbar.js
@@ -3,16 +3,7 @@ var page = document.getElementById('title').innerText
 var usuario = document.getElementById('usuario').innerText
 document.getElementById('usuario').innerHTML = ""
 
-
-
-if (usuario == 'AnonymousUser'){
-    document.getElementById('list').innerHTML = 
-        '<li id="index"><a href="/index/">Home</a></li>'+
-        '<li class="LoginOut" id="login"><a href="/user/login/">Login</a></li>'+
-        '<li class="LoginOut" id="signin"><a href="/user/signin/">Sign In</a></li>' +
-        '<li id="userinfo"><a href="/user/info">User info temp</a></li>'
-    console.log(usuario)
-    pagina = document.getElementById('title').innerText
+function activate_page(pagina){
     if (pagina === 'Home'){
         document.getElementById('index').innerHTML = 
         '<a href="/index/" class="active">Home</a>'
@@ -25,15 +16,24 @@ if (usuario == 'AnonymousUser'){
         document.getElementById('signin').innerHTML = 
         '<a href="/user/signin/" class="active">Sign In</a>'
     }
-    else{
-        console.log(pagina)
+    else if (pagina === 'User Info'){
+        document.getElementById('userinfo').innerHTML = 
+        '<a href="/user/info" class="active">Logado como: '+ usuario +'</a>'
     }
+}
+
+if (usuario == 'AnonymousUser'){
+    document.getElementById('list').innerHTML = 
+        '<li id="index"><a href="/index/">Home</a></li>'+
+        '<li class="LoginOut" id="login"><a href="/user/login/">Login</a></li>'+
+        '<li class="LoginOut" id="signin"><a href="/user/signin/">Sign In</a></li>' +
+        '<li id="userinfo"><a href="/user/info">User info temp</a></li>'
+    activate_page(document.getElementById('title').innerText)
 }
 else{
     document.getElementById('list').innerHTML=
-        '<li><a href="/index/" id="index">Home</a></li>'+
-        '<li class="LoginOut"><a href="/user/logoff">Logout</a></li>' +
-        '<li class="LoginOut"><a href="/user/info">Logado como: '+ usuario +'</a></li>'+
-        '<li id="userinfo"><a href="/user/info">User info temp</a></li>'
-        console.log(page + " " + usuario)
+        '<li id="index"><a href="/index/">Home</a></li>'+
+        '<li id="logoff" class="LoginOut"><a href="/user/logoff">Logout</a></li>' +
+        '<li id="userinfo" class="LoginOut"><a href="/user/info">Logado como: '+ usuario +'</a></li>'
+    activate_page(document.getElementById('title').innerText)
 }
