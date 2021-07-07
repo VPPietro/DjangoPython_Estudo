f3e60542c584ef7432f6e539e320c34184b9e3c8
pietro.vanelli <pietroparaventivanelli@gmail.com>
2021-07-07 10:24:12 -0300

    ativar pagina final (melhor)

--- mercadolivre/mercadolivre/static/headbar.js
+++ mercadolivre/mercadolivre/static/headbar.js
@@ -3,29 +3,20 @@ var page = document.getElementById('title').innerText
 var usuario = document.getElementById('usuario').innerText
 document.getElementById('usuario').innerHTML = ""
 
-function activate_page2(pagina){
-    if (pagina === 'Home'){
-        document.getElementById('index').innerHTML = 
-        '<a href="/index/" class="active">Home</a>'
-    }
-    else if (pagina === 'Login'){
-        document.getElementById('login').innerHTML =
-        '<a href="/user/login/" class="active">Login</a>'
-    }
-    else if (pagina === 'Signin'){
-        document.getElementById('signin').innerHTML = 
-        '<a href="/user/signin/" class="active">Sign In</a>'
-    }
-    else if (pagina === 'User Info'){
-        document.getElementById('userinfo').innerHTML = 
-        '<a href="/user/info" class="active">Logado como: '+ usuario +'</a>'
-    }
-}
 
 function activate_page(pagina){
-    if (pagina === '/index/'){
-    var activate = document.querySelector('#index').querySelector('a')
-    activate.setAttribute('class', 'active')
+    console.log(pagina)
+    if (pagina === '/index/' || pagina === '/'){
+        document.querySelector('#index').querySelector('a').setAttribute('class', 'active')
+    }
+    else if (pagina === '/user/signin/'){
+        document.querySelector('#signin').querySelector('a').setAttribute('class', 'active')
+    }
+    else if (pagina === '/user/login/'){
+        document.querySelector('#login').querySelector('a').setAttribute('class', 'active')
+    }
+    else if (pagina === '/user/info/'){
+        document.querySelector('#userinfo').querySelector('a').setAttribute('class', 'active')
     }
 }
 
@@ -42,5 +33,5 @@ else{
         '<li id="index"><a href="/index/">Home</a></li>'+
         '<li id="logoff" class="LoginOut"><a href="/user/logoff">Logout</a></li>' +
         '<li id="userinfo" class="LoginOut"><a href="/user/info">Logado como: '+ usuario +'</a></li>'
-    activate_page(document.getElementById('title').innerHTML)
+    activate_page(location.pathname)
 }
