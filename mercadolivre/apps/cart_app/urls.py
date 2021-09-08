f327a6c997feaebe653efe4dc9f143304a9f4d7b
Pietro Paraventi <pietroparaventivanelli@gmail.com>
2021-09-08 20:44:50 -0300

    up

--- mercadolivre/apps/cart_app/urls.py
+++ mercadolivre/apps/cart_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from apps.cart_app import views
 
 urlpatterns = [
-    path('', views.CartView.as_view(), name='cart_page')
+    path('', views.CartView.as_view(), name='cart_page'),
+    # path('add)
 ]
