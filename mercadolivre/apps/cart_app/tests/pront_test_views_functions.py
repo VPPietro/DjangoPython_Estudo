6ed00360c5ec86d997f4de23c39a8c80eb2b64cc
Pietro Paraventi <pietroparaventivanelli@gmail.com>
2021-09-30 21:07:17 -0300

    up cart_app tests

--- mercadolivre/apps/cart_app/tests/pront_test_views_functions.py
+++ mercadolivre/apps/cart_app/tests/pront_test_views_functions.py
@@ -10,7 +10,7 @@ class get_or_create_cartTest(TestCase):
     def setUp(self) -> None:
         setup_std(self, user=True, anonimo=True)
 
-    def tes_request_com_somente_cart_anonimo(self):
+    def test_request_com_somente_cart_anonimo(self):
         """Verifica se a função retorna False e obj cart_anonimo"""
         # define uma request somente com cart anonimo
         request = set_request(self, reverse_lazy('cart_page'))
@@ -20,7 +20,7 @@ class get_or_create_cartTest(TestCase):
         # verifica se retorna False, obj cart_anonimo
         self.assertEqual(not retorno[0], isinstance(retorno[1], CartModel))
 
-    def tes_request_com_somente_cart_user(self):
+    def test_request_com_somente_cart_user(self):
         """Verifica se a função retorna obj cart_user e False"""
         # define uma request somente com cart user
         request = set_request(self, reverse_lazy('cart_page'), user=self.user)
@@ -29,7 +29,7 @@ class get_or_create_cartTest(TestCase):
         # verifica se retorna obj cart_user e False
         self.assertEqual(isinstance(retorno[0], CartModel), not retorno[1])
 
-    def tes_request_com_os_dois_carts(self):
+    def test_request_com_os_dois_carts(self):
         """Verifica se a função retorna obj cart_user e obj cart_anonimo"""
         # define uma request com os dois carts
         request = set_request(self, reverse_lazy('cart_page'), user=self.user)
@@ -42,7 +42,7 @@ class get_or_create_cartTest(TestCase):
 
 class get_cart_itemsTest(TestCase):
 
-    def tes_manda_carrinho_com_itens(self):
+    def test_manda_carrinho_com_itens(self):
         """Verifica se ao mandar carrinho com itens, é retornado um queryset
         com cart_items"""
         # cria um carrinho com itens
@@ -54,7 +54,7 @@ class get_cart_itemsTest(TestCase):
         # verifica se retornou o item dentro do queryset(queryset vazio retorna False)
         self.assertTrue(retorno)
 
-    def tes_manda_carrinho_sem_itens(self):
+    def test_manda_carrinho_sem_itens(self):
         """Verifica se ao mandar carrinho sem itens, é retornado um queryset vazio"""
         # cria um carrinho sem itens
         carrinho = CartModel.objects.create()
@@ -68,7 +68,7 @@ class ajusta_carrinhoTest(TestCase):
     def setUp(self) -> None:
         setup_std(self, user=True, anonimo=True)
 
-    def tes_user_anonimo_com_carrinho(self):
+    def test_user_anonimo_com_carrinho(self):
         """Verifica se a função retorna o carrinho do user anonimo sem alterações"""
         # define uma request com user anonimo com carrinho
         request = set_request(self, '')
@@ -81,7 +81,7 @@ class ajusta_carrinhoTest(TestCase):
         self.assertEqual(retorno[1].cart_item.all()[0], self.carrinho_anonimo.cart_item.all()[0])
         self.assertEqual(retorno[1].cart_item.all()[0].quantidade_compra, self.carrinho_anonimo.cart_item.all()[0].quantidade_compra)
 
-    def tes_user_anonimo_sem_carrinho(self):
+    def test_user_anonimo_sem_carrinho(self):
         """Verifica se a função retorna um novo carrinho para o user anonimo"""
         # define uma request com user anonimo e sem carrinho
         request = set_request(self, '')
@@ -90,7 +90,7 @@ class ajusta_carrinhoTest(TestCase):
         # verifica se retorna um novo carrinho para o user anonimo
         self.assertTrue(isinstance(retorno[1], CartModel))
 
-    def tes_user_logado_sem_carrinho_anonimo(self):
+    def test_user_logado_sem_carrinho_anonimo(self):
         """Verifica se a função retorna o carrinho do user sem alterações"""
         # define uma request com user logado, sem carrinho anonimo
         request = set_request(self, '', user=self.user)
@@ -100,7 +100,7 @@ class ajusta_carrinhoTest(TestCase):
         self.assertEqual(retorno[0].cart_item.all()[0], self.carrinho_do_user.cart_item.all()[0])
         self.assertEqual(retorno[0].cart_item.all()[0].quantidade_compra, self.carrinho_do_user.cart_item.all()[0].quantidade_compra)
 
-    def tes_user_logado_com_carrinho_anonimo(self):
+    def test_user_logado_com_carrinho_anonimo(self):
         """Verifica se a função retorna o carrinho do user junto com os itens do carrinho anonimo"""
         # define uma request com user logado, com carrinho anonimo
         request = set_request(self, '', user=self.user)
@@ -118,14 +118,14 @@ class ajusta_carrinhoTest(TestCase):
 
 class cria_carrinhoTest(TestCase):
 
-    def tes_cria_carrinho_anonimo(self):
+    def test_cria_carrinho_anonimo(self):
         """Cria um carrinho sem um user, ou seja, anonimo"""
         # chama a função sem nenhum valor informado
         retorno = cria_carrinho()
         # verifica se é retornado um CartModel sem usuário
         self.assertTrue(isinstance(retorno, CartModel))
 
-    def tes_cria_carrinho_com_user(self):
+    def test_cria_carrinho_com_user(self):
         """Cria um carrinho com um user"""
         setup_std(self, user_sem_carrinho=True)
         # chama a função informando um user
@@ -138,7 +138,7 @@ class deleta_carrinhoTest(TestCase):
     def setUp(self) -> None:
         setup_std(self, user=True)
 
-    def tes_deleta_carrinho(self):
+    def test_deleta_carrinho(self):
         """Verifica se a função deleta um carrinho"""
         # manda um carrinho para a função
         deleta_carrinho(self.carrinho_do_user)
@@ -150,7 +150,7 @@ class join_cartsTest(TestCase):
     def setUp(self) -> None:
         setup_std(self, user=True,anonimo=True)
 
-    def tes_manda_carrinho_user_vazio_e_anonimo_com_item(self):
+    def test_manda_carrinho_user_vazio_e_anonimo_com_item(self):
         """Verifica se será adicionado os itens do cart anonimo para o cart do user"""
         # manda um carrinho vazio e um carrinho anonimo com item para a função
         carrinho_user = CartModel.objects.create()
@@ -161,7 +161,7 @@ class join_cartsTest(TestCase):
         self.assertEqual(item_user.loja_item.id, item_anonimo.loja_item.id)
         self.assertEqual(item_user.quantidade_compra, item_anonimo.quantidade_compra)
 
-    def tes_manda_carrinho_do_user_com_item_e_anonimo_vazio(self):
+    def test_manda_carrinho_do_user_com_item_e_anonimo_vazio(self):
         """A função não deve fazer nada, pois não é necessário alterar"""
         # manda um carrinho com itens e um carrinho anonimo vazio para a função
         carrinho_anonimo = CartModel.objects.create()
@@ -172,7 +172,7 @@ class join_cartsTest(TestCase):
         self.assertEqual(item_original.loja_item.id, item_pos_join.loja_item.id)
         self.assertEqual(item_original.quantidade_compra, item_pos_join.quantidade_compra)
 
-    def tes_manda_todos_carrinhos_com_itens(self):
+    def test_manda_todos_carrinhos_com_itens(self):
         """Verifica se será adicionado os itens do cart anonimo para o cart do user"""
         # verifica se o user só tem um item inicialmente no carrinho
         itens_user_original = self.carrinho_do_user.cart_item.all()
@@ -193,7 +193,7 @@ class add_to_cart_funcTest(TestCase):
         setup_std(self, user=True)
         self.itens_carrinho = self.carrinho_do_user.cart_item.all()
 
-    def tes_manda_item_existente_no_carrinho(self):
+    def test_manda_item_existente_no_carrinho(self):
         """Verifica se ao mandar um item que já existe no carrinho
         o mesmo não será duplicado, mas sim, terá sua quantidade aumentada"""
         self.assertEqual(self.itens_carrinho[0].quantidade_compra, 333)
@@ -213,4 +213,5 @@ class add_to_cart_funcTest(TestCase):
         # verifica se o item não foi duplicado
         self.assertEqual(len(self.itens_carrinho), 2)
         # verifica se o item correto foi adicionado
-        self.assertEqual()
+        self.assertEqual(self.itens_carrinho[1].loja_item.id, self.item_user2.id)
+        self.assertEqual(self.itens_carrinho[1].quantidade_compra, 1)
